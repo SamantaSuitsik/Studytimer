32374dd216da1636fb050130a1a12781cbb0d0d4
Samanta <samanta.suitsik@gmail.com>
2023-07-29 17:10:35 +0300

    must taust, lõpetamise nupp ja aegade algne ekraan

--- pomodoro/src/main/java/com/example/pomodoro/HelloApplication.java
+++ pomodoro/src/main/java/com/example/pomodoro/HelloApplication.java
@@ -4,12 +4,15 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -17,33 +20,39 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+//varvid: roosa - EDCBD2, heledaim roheline - 80C4B7, kesk roh - 369a89
+//tumeroh - 1b6a63
+
 
 public class HelloApplication extends Application {
     private boolean jookseb = false;
-    private Taimer taimer;
+    private Taimer taimer = null;
 
     @Override
     public void start(Stage stage) {
         BorderPane border = new BorderPane();
-
+        //border.setId("border");
         TextField kirjutuskast = new TextField("00");
         TextField kirjutuskast2 = new TextField("00");
         TextField kirjutuskast3 = new TextField("00");
 
-
-
+        //lõpetamise nupp
+        Button lopuNupp = new Button("Lõpeta");
+        lopuNupp.setPadding(new Insets(1,5,1,5));
+        lopuNupp.setVisible(false);//peidame alguses
 
         //lisab borderpaneile regioonid
-        border.setBottom(addHBoxAlumine(kirjutuskast));
-        border.setCenter(keskOsa(kirjutuskast,kirjutuskast2,kirjutuskast3));
-        border.setStyle("-fx-background-color: #EDCBD2;");
+        border.setBottom(addHBoxAlumine(lopuNupp)); //tõstab gridpanei kõrgemale!
+        border.setCenter(keskOsa(kirjutuskast,kirjutuskast2,kirjutuskast3,lopuNupp));
+        border.setStyle("-fx-background-color: #1a1a1c;");
 
+        //border.setBackground(new Background(bg));
         Scene scene = new Scene(border, 280,440);
         //scene.getStylesheets().add(getClass().getResource("stylesheet.css").toString());
         //lisame scene'ile css-i
@@ -56,12 +65,45 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
-    public HBox addHBoxAlumine(TextField kirjutuskast) {
-        HBox hbox = new HBox();
-        hbox.setPadding(new Insets(1, 1, 25, 1));
-        hbox.setSpacing(110);
-        hbox.setAlignment(Pos.CENTER);
-        hbox.setPrefSize(440,100);
+    public VBox addHBoxAlumine(Button lopuNupp) {
+        VBox vbox = new VBox();
+        vbox.setPadding(new Insets(15, 0, 0, 0));
+        vbox.setSpacing(40);
+        vbox.setAlignment(Pos.BASELINE_CENTER);
+        vbox.setPrefSize(440,100);
+
+        //tab bar
+        HBox tabbar = new HBox();
+        tabbar.setAlignment(Pos.CENTER);
+        tabbar.setPadding(new Insets(1,1,1,1));
+        tabbar.setMaxHeight(23);
+        tabbar.setMinHeight(23);
+        tabbar.setStyle("-fx-background-color: #151516;");
+        //aegade nupp
+        Button aegadeNupp = new Button();
+        aegadeNupp.setPadding(new Insets(0,0,0,0));//hitbox on vaiksem nupul
+        aegadeNupp.getStyleClass().add("aegadeNupp");
+        tabbar.getChildren().add(aegadeNupp);
+
+        //nupp kolmnurga pildiks
+        ImageView pilt = new ImageView(new Image(getClass().getResourceAsStream("lillakolmnurk.png")));
+        pilt.setFitHeight(18);
+        pilt.setFitWidth(20);
+        aegadeNupp.setGraphic(pilt);
+        aegadeNupp.setOnMouseEntered(event -> {
+            aegadeNupp.setText("Ajad");
+
+        });
+        aegadeNupp.setOnMouseExited(event ->
+                aegadeNupp.setText(""));
+
+        aegadeNupp.setOnMouseClicked(event -> {
+            try {
+                aegadeEkraan();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
 
         Polygon kolmnurk = new Polygon();
         kolmnurk.getPoints().addAll(
@@ -73,6 +115,7 @@ public class HelloApplication extends Application {
         ruut.setHeight(30);
         ruut.setWidth(30);
 
+        //nupud pole hetkel kasutusel!
         Button kolmnurkNupp = new Button();
         kolmnurkNupp.setShape(kolmnurk);
         kolmnurkNupp.setStyle("-fx-border-color: #d56b57; -fx-border-width: 1px; -fx-background-color: #E3856B; -fx-padding: 35 0 0 0; -fx-font-size: 13px; " +
@@ -87,20 +130,53 @@ public class HelloApplication extends Application {
                 "-fx-text-fill: #8f3a28; -fx-font-family: Norasi");
         ruutNupp.setText("Muu");
 
-        Color oranz = Color.web("#c15b44");
 
-        //ettevalmistused helendamiseks
+        //ettevalmistused helendamiseks hoverides
+        Color oranz = Color.web("#c15b44");
+        Color valge = Color.web("#bababa");
         DropShadow dropShadow = new DropShadow(0, oranz);
         DropShadow dropShadow2 = new DropShadow(0, oranz);
+        DropShadow shadowLopuNupule = new DropShadow(0,valge);
         ruutNupp.setEffect(dropShadow);
         kolmnurkNupp.setEffect(dropShadow2);
+        lopuNupp.setEffect(shadowLopuNupule);
 
         ruutNupp.setOnMouseEntered(event -> animateGlowEffect(ruutNupp));
         kolmnurkNupp.setOnMouseEntered(event -> animateGlowEffect(kolmnurkNupp));
+        lopuNupp.setOnMouseEntered(event -> animateGlowEffect(lopuNupp));
 
-        //hbox.getChildren().addAll(kolmnurkNupp,ruutNupp);
-        hbox.getChildren().addAll(kirjutuskast);
-        return hbox;
+        vbox.getChildren().addAll(lopuNupp,tabbar);
+        return vbox;
+    }
+
+    private void aegadeEkraan() throws Exception {
+        //votame ajad Taimeri klassi abiga
+        ArrayList<String> ajad = Taimer.loeFailist();
+        System.out.println(ajad);
+
+        VBox vbox = new VBox();
+        vbox.getStyleClass().add("aegadeLeht");
+        vbox.setPadding(new Insets(15, 10, 15, 10));
+        vbox.setSpacing(10);
+        vbox.setAlignment(Pos.BASELINE_CENTER);
+        vbox.setPrefSize(440,280);
+
+        //lisatakse ajad ekraanile
+        for (int i = 0; i < ajad.size(); i++) {
+            String aeg = ajad.get(i);
+            Text ajaTekst = new Text(aeg);
+            ajaTekst.setStyle("-fx-fill: #e6e6e6; -fx-font-family: Norasi;-fx-font-size: 20");
+            vbox.getChildren().add(ajaTekst);
+        }
+
+        //uus stage ja scene
+        Stage stageAjad = new Stage();
+        Scene scene = new Scene(vbox,280,440);
+        //lisame scene'ile css-i
+        scene.getStylesheets().add(this.getClass().getResource("stylesheet.css").toExternalForm());
+        stageAjad.setTitle("Ajad");
+        stageAjad.setScene(scene);
+        stageAjad.show();
     }
 
     public void animateGlowEffect(Button button) {
@@ -108,7 +184,7 @@ public class HelloApplication extends Application {
 
         Timeline timeline1 = new Timeline(
                 new KeyFrame(Duration.ZERO, new KeyValue(dropShadow.radiusProperty(), 0)),
-                new KeyFrame(Duration.seconds(0.1), new KeyValue(dropShadow.radiusProperty(), 25))
+                new KeyFrame(Duration.seconds(0.1), new KeyValue(dropShadow.radiusProperty(), 18))
         );
         timeline1.setAutoReverse(true);
         button.setOnMouseExited(event -> {
@@ -120,13 +196,17 @@ public class HelloApplication extends Application {
         timeline1.play();
     }
 
-    public GridPane keskOsa(TextField tunnid, TextField minutid, TextField sekundid) {
+    public GridPane keskOsa(TextField tunnid, TextField minutid, TextField sekundid, Button lopuNupp) {
         Text koolon = new Text(":");
         Text koolon2 = new Text(":");
-        koolon.setFill(Paint.valueOf("#1b6a63"));
-        koolon2.setFill(Paint.valueOf("#1b6a63"));
+        koolon.setFill(Paint.valueOf("#e6e6e6"));
+        koolon2.setFill(Paint.valueOf("#e6e6e6"));
         koolon.getStyleClass().add("koolon");
         koolon2.getStyleClass().add("koolon");
+
+        Label eesmark = new Label("Eesmärk");
+        eesmark.getStyleClass().add("label");
+
         //gridpane ja selle joondamine
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);// Set alignment to center
@@ -152,41 +232,81 @@ public class HelloApplication extends Application {
         Circle ring = new Circle(100);
         Button ringNupp = new Button();
         ringNupp.setShape(ring);
-        ringNupp.setPrefSize(200,200);
-        ringNupp.setText("Alusta");
-        ringNupp.setStyle("-fx-background-color: #80C4B7; -fx-border-color: #369a89; -fx-border-width: 2px;" +
-                "-fx-font-size: 38px; -fx-text-fill: #1b6a63; -fx-font-family: Norasi; ");
+        ringNupp.setPrefSize(200, 200);
+        ringNupp.setText("   Sea\neesmärk!");
+        ringNupp.setStyle("-fx-background-color: #e6e6e6; -fx-border-color: #bababa; -fx-border-width: 2px;" +
+                "-fx-font-size: 38px; -fx-text-fill: #1a1a1c; -fx-font-family: Norasi; ");
         //helenduse ettevalmistus hoverides
-        DropShadow dropShadow = new DropShadow(0, Color.web("#1b6a63"));
+        DropShadow dropShadow = new DropShadow(0, Color.web("#B2B1B1A8"));
         ringNupp.setEffect(dropShadow);
         ringNupp.setOnMouseEntered(event ->
-            animateGlowEffect(ringNupp)
+                animateGlowEffect(ringNupp)
         );
 
         ringNupp.setOnMouseClicked(e -> {
-            //kui taimer juba tehtud on ja see jookseb praegu (ei tee uut taimerit)
-            if (jookseb)
-                taimer.kaivita();
-            else {
-                //kui taimerit pole ja seega see ka ei jookse, siis loome selle
-                jookseb = true;
-                Taimer taimer = new Taimer(ringNupp,tunnid,minutid,sekundid);
-
-                if (taimer.getKasVoibAlustada()) //kas see on hea idee????
-                    taimer.kaivita();
-            }
+            System.out.println("Klikk HelloAPpis");
+            taimeriKaivituseAlgus(ringNupp, lopuNupp, tunnid, minutid, sekundid, koolon, koolon2, eesmark);
         });
+
+        lopuNupp.setOnMouseClicked(event1 -> {
+            taimeriPeatamine(lopuNupp,tunnid,minutid,sekundid,koolon,koolon2,eesmark);
+        });
+
+        //paneme gridile objektid
         stackpane.getChildren().add(ringNupp);
-        grid.add(stackpane, 0, 0,9,1);
-        grid.add(tunnid,2,1);
+        grid.add(stackpane, 0, 0, 9, 1);
+        grid.add(tunnid, 2, 1);
         grid.add(minutid, 4, 1);
-        grid.add(sekundid,6,1);
-        grid.add(koolon,3,1);
-        grid.add(koolon2,5,1);
+        grid.add(sekundid, 6, 1);
+        grid.add(koolon, 3, 1);
+        grid.add(koolon2, 5, 1);
+        grid.add(eesmark, 4, 1, 3, 1);
+        GridPane.setHalignment(eesmark, HPos.RIGHT);
+        GridPane.setValignment(eesmark, VPos.BOTTOM);
         //GridPane.setHalignment(ring, javafx.geometry.HPos.CENTER); // Align the circle within the cell
         return grid;
     }
 
+    public void taimeriKaivituseAlgus(Button ringNupp, Button lopuNupp, TextField tunnid, TextField minutid, TextField sekundid, Text koolon, Text koolon2, Label eesmark) {
+        //kui taimer juba tehtud on ja see jookseb praegu (ei tee uut taimerit)
+        //paneme pausile kaivita meetodi abiga
+        if (jookseb)
+            taimer.kaivita();
+
+        else {
+            //kui taimerit pole ja seega see ka ei jookse, siis loome selle
+            //ringNupp.setText("Alusta");//jookseb yle siit prgu
+            taimer = new Taimer(ringNupp, lopuNupp, tunnid, minutid, sekundid);
+            taimer.kaivita();
+            lopuNupp.setVisible(true);
+            jookseb = true;
+
+            //soovitud aega ei saa enam muuta ja eesmärgi värv muutub
+            tunnid.setDisable(true);
+            minutid.setDisable(true);
+            sekundid.setDisable(true);
+            koolon.setFill(Paint.valueOf("#815db8"));
+            koolon2.setFill(Paint.valueOf("#815db8"));
+            eesmark.setStyle("-fx-text-fill: #766896");
+        }
+    }
+
+    private void taimeriPeatamine(Button lopuNupp, TextField tunnid, TextField minutid, TextField sekundid, Text koolon, Text koolon2, Label eesmark) {
+        try {
+            taimer.stop();
+            jookseb = false;
+            lopuNupp.setVisible(false);
+            //eesmarki saab uuesti muuta
+            tunnid.setDisable(false);
+            minutid.setDisable(false);
+            sekundid.setDisable(false);
+            koolon.setFill(Paint.valueOf("#e6e6e6"));
+            koolon2.setFill(Paint.valueOf("#e6e6e6"));
+            eesmark.setStyle("-fx-text-fill: #e6e6e6");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String[] args) {
         launch();
--- pomodoro/src/main/java/com/example/pomodoro/Taimer.java
+++ pomodoro/src/main/java/com/example/pomodoro/Taimer.java
@@ -7,6 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.util.Duration;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 public class Taimer {
@@ -19,32 +23,36 @@ public class Taimer {
     private long algusPausil;
     private long duration;
     private Button nupp;
-    private boolean kasVoibAlustada;
+    private Button lopuNupp;
 
-    public Taimer(Button nupp, TextField tunnid, TextField minutid, TextField sekundid) {
-        this.duration = ajaKalkulaator(tunnid,minutid,sekundid);
+    public Taimer(Button nupp, Button lopuNupp, TextField tunnid, TextField minutid, TextField sekundid) {
+        this.duration = ajaKalkulaator(tunnid, minutid, sekundid);
         if (duration == -1) {
             //kui sisestus oli vale
             nupp.setText("  Vale\nsisestus");
             tunnid.setText("00");
             minutid.setText("00");
             sekundid.setText("00");
-            kasVoibAlustada = false;
+
         }
-        else
-            kasVoibAlustada = true;
+        //kui eesmärki ei seatud, siis on see 2h
+        else if (duration == 0) {
+            tunnid.setText("02");
+            duration = 4800;
+        }
+
         this.nupp = nupp;
+        this.lopuNupp = lopuNupp;
         this.algusAeg = System.currentTimeMillis();
         this.jookseb = false;
         this.timeText = "00:00:00";
         this.elapsedTime = 0;
         this.algusPausil = 0;
 
-    }
+        //kui vajutatakse lopunuppu
 
-    public boolean getKasVoibAlustada() {
-        //pohiprogramm kysib seda enne alustamist sest voimalik et sisestus on vale
-        return kasVoibAlustada;
+
+
     }
 
     public long ajaKalkulaator(TextField kirjutuskast, TextField kirjutuskast2, TextField kirjutuskast3) {
@@ -52,23 +60,22 @@ public class Taimer {
         int sekundid = -1;
         //kasutatakse regexit et tuvastada kas sisend on numbriline
         Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
-            if (pattern.matcher(kirjutuskast.getText()).matches() &&
-                    pattern.matcher(kirjutuskast2.getText()).matches() &&
-                    pattern.matcher(kirjutuskast3.getText()).matches()) {
-                System.out.println("kontroll korras");
+        if (pattern.matcher(kirjutuskast.getText()).matches() &&
+                pattern.matcher(kirjutuskast2.getText()).matches() &&
+                pattern.matcher(kirjutuskast3.getText()).matches()) {
+            System.out.println("kontroll korras");
 
-                int tunnid = Integer.parseInt(kirjutuskast.getText());
-                int minutid = Integer.parseInt(kirjutuskast2.getText());
-                sekundid = Integer.parseInt(kirjutuskast3.getText());
+            int tunnid = Integer.parseInt(kirjutuskast.getText());
+            int minutid = Integer.parseInt(kirjutuskast2.getText());
+            sekundid = Integer.parseInt(kirjutuskast3.getText());
 
-                if (minutid > 59 || sekundid > 59) {
-                    //vale sisestus
-                    sekundid = -1;
-                }
-                else {
-                    //kui sisestus on õige - teisendus sekunditeks
-                    sekundid = sekundid + (minutid*60) + (tunnid*3600);
-                }
+            if (minutid > 59 || sekundid > 59) {
+                //vale sisestus
+                sekundid = -1;
+            } else {
+                //kui sisestus on õige - teisendus sekunditeks
+                sekundid = sekundid + (minutid * 60) + (tunnid * 3600);
+            }
 
         }
         return sekundid;
@@ -76,11 +83,7 @@ public class Taimer {
     }
 
     public void kaivita() {
-        taimeriALgus();
-    }
-
-    private void taimeriALgus() {
-        nupp.setOnAction(event -> {
+        //teine kord ta votab ainult selle kliki
             if (jookseb) {
                 //kui taimer parasjagu käib -> tuleb paus
                 timeline.stop();
@@ -88,8 +91,7 @@ public class Taimer {
                 oliPausil = true;
                 jookseb = false;
                 System.out.println("paus");
-            }
-            else if (oliPausil) {
+            } else if (oliPausil) {
                 //leiame kui kaua oli pausil
                 long pausilAeg = System.currentTimeMillis() - algusPausil;
                 //lisame algusajale pausil oldud aja, et timer algaks samast kohast
@@ -99,19 +101,15 @@ public class Taimer {
                 oliPausil = false;
                 //oliPausil on tõene ja hakkab jooksma
                 System.out.println("votsin pausilt ara");
-            }
-            else {
+            } else {
                 //kui taimer ei käi ega pole pausil
                 jookseb = true;
-                System.out.println("tere");
+                System.out.println("uus taimer laks kaima");
                 algusAeg = System.currentTimeMillis();
                 //nupp.setDisable(true); //et sekundeid midagi ei häiriks (teeb tumedaks)
                 updateTimeElapsed(nupp);
             }
-        });
-
     }
-
 
     private void updateTimeElapsed(Button nupp) {
         long durationSec = duration + 1;
@@ -141,6 +139,42 @@ public class Taimer {
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
+
+    }
+
+    public void stop() throws Exception {
+        if (jookseb) {
+            timeline.stop();
+            System.out.println("TA TULI LABI SIIT");
+            oliPausil = false;
+            jookseb = false; //vaja, et tehtaks käivitades uus taimer
+            kirjutaFaili();//salvestame aja
+            TimeUnit.SECONDS.sleep(1);//et naha oma aega veel korraks
+            nupp.setText("Alusta");
+
+        }
+
+    }
+
+    private void kirjutaFaili() throws Exception {
+        //kirjutab praeguse aja faili
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("ajad.txt",true))) {
+            writer.write(timeText + "\n");
+        }
+    }
+
+    public static ArrayList<String> loeFailist() throws Exception {
+        ArrayList<String> ajad = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("ajad.txt"), StandardCharsets.UTF_8))) {
+            String aeg = reader.readLine();
+            while (aeg != null) {
+                //loeme ajad listi
+                ajad.add(aeg);
+                aeg = reader.readLine();
+            }
+        }
+        return ajad;
 
     }
 
